3b458cd128ab16841f78e49273f2117b82bfa198
Ali Grivani <ali.gerivani@gmail.com>
2025-08-11 21:52:00 -0400

    Add release workflow

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
-
+import os
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
     name='gym-playground',
-    version='0.3.0',
+    version='0.3.1',
+    author='Ali Grivani',
+    description='A package for running and testing various gym environments.',
+    long_description=open('README.md').read() if os.path.exists('README.md') else '',
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=requirements,
     entry_points={
